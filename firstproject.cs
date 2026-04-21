050bc9412ef4ad33416dbf2ff2a0e92241d355a0
Spondon33 <nurulaminkh33@gmail.com>
2026-04-21 23:56:17 +0600

    Boolean Types

--- firstproject.cs
+++ firstproject.cs
@@ -11,25 +11,11 @@ namespace firstproject
     {
         static void Main(string[] args)
         {
-            string textage = "25";
-            int age = Convert.ToInt32(textage);
-            Console.WriteLine(age);
-            
-            String textpopulation = "7800000000";
-            long population = Convert.ToInt64(textpopulation);
-            Console.WriteLine(population);
-            
-            String textprice = "19.99";
-            double price = Convert.ToDouble(textprice);
-            Console.WriteLine(price);
-            
-            String texttemperature = "36.5";
-            float temperature = Convert.ToSingle(texttemperature);
-            Console.WriteLine(temperature);
-            
-            String textsalary = "50000.00";
-            decimal salary = Convert.ToDecimal(textsalary);
-            Console.WriteLine(salary);
+            bool value = true;
+            Console.WriteLine(value);
+
+            value = false;
+            Console.WriteLine(value);
         }
     }
 }
