dc67d82e8778eecb6a765ac738d41e04e26da1d0
Spondon33 <nurulaminkh33@gmail.com>
2026-04-30 13:37:30 +0600

    Numeric Formatting

--- Learning.cs
+++ Learning.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Channels;
@@ -12,10 +13,25 @@ namespace firstproject
     {
         static void Main(string[] args)
         {
-            int age = -10;
-            // condition ? true result : false result
-            string result = age > 0 ? "Age is valid" : "Age is invalid";
-            Console.WriteLine(result);
+            double value = 1000 / 12.34;
+            Console.WriteLine(value);
+            Console.WriteLine(String.Format("{0:0}", value));
+            Console.WriteLine(String.Format("{0:0.0}", value));
+            Console.WriteLine(String.Format("{0:0.00}", value));
+            Console.WriteLine(String.Format("{0:0.#}", value));
+
+            double money = -10d / 3d;
+            Console.WriteLine(money);
+            Console.WriteLine(String.Format("${0:0.00}", money));
+            Console.WriteLine(money.ToString("C"));
+            Console.WriteLine(money.ToString("C0"));
+            Console.WriteLine(money.ToString("C1"));
+            Console.WriteLine(money.ToString("C2"));
+
+            Console.WriteLine(money.ToString("C", CultureInfo.CurrentCulture));
+            Console.WriteLine(money.ToString("C", CultureInfo.CreateSpecificCulture("en-US")));
+            Console.WriteLine(money.ToString("C", CultureInfo.CreateSpecificCulture("en-GB")));
+
         }
     }
 }
